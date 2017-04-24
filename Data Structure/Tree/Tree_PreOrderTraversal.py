9d5372904e08685da57b3636cbd28dba0844172f
Shun Lin <s92014721@berkeley.edu>
2017-04-24 09:53:25 -0700

    added Tree section and tree_preOrderTraversal.py

---
+++ Data Structure/Tree/Tree_PreOrderTraversal.py
@@ -0,0 +1,15 @@
+"""
+Node is defined as
+self.left (the left child of the node)
+self.right (the right child of the node)
+self.data (the value of the node)
+"""
+def preOrder(root):
+    #Write your code here
+    if (root == None):
+        return;
+    print(root.data, end=" ");
+    if (root.left):
+        preOrder(root.left);
+    if (root.right):
+        preOrder(root.right);
