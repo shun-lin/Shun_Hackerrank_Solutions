bc7ff9d2a0a2bde5bd827bc4581b9db28ee31a60
Shun Lin <s92014721@berkeley.edu>
2017-04-24 10:19:32 -0700

    added postordertraversal.py

---
+++ Python/Tree_PostOrderTraversal.py
@@ -0,0 +1,15 @@
+"""
+Node is defined as
+self.left (the left child of the node)
+self.right (the right child of the node)
+self.data (the value of the node)
+"""
+def postOrder(root):
+    #Write your code here
+    if (root == None):
+        return;
+    if (root.left):
+        postOrder(root.left);
+    if (root.right):
+        postOrder(root.right);
+    print(root.data, end=" ");
